acbccafe66ef5fc4b85b418b5ba096c57d0b3a98
Edward Jiang <edward@stormpath.com>
2016-11-21 14:49:00 -0800

    updated naming for oauth echo credentials

--- Sources/Turnstile/Credentials/OAuthEcho.swift
+++ Sources/Turnstile/Credentials/OAuthEcho.swift
@@ -8,12 +8,30 @@
 
 import Foundation
 
+/**
+ The OAuth Echo credential represents the info sent to the server for delegated OAuth 1.0a
+ This is an extension mainly used by Twitter/Digits
+ See: 
+  * https://dev.twitter.com/oauth/echo
+  * http://oauthbible.com/#oauth-10a-echo
+  * https://docs.fabric.io/apple/digits/advanced-setup.html#verifying-a-user
+ */
 public class OAuthEcho: Credentials {
-    public let requestURL: URL
-    public let authorizationHeader: String
-
-    public init(requestURL: URL, authorizationHeader: String) {
-        self.requestURL = requestURL
-        self.authorizationHeader = authorizationHeader
+    /**
+     The auth provider for this set of credentials. Usually passed via
+     the `X-Auth-Service-Provider` header
+     */
+    public let authServiceProvider: URL
+    
+    /**
+     The OAuth parameters passed in through either the `X-Verify-Credentials-Authorization`
+     header or body parameters.
+     */
+    public let oauthParameters: String
+    
+    /// Initializer for OAuth Echo credential data
+    public init(authServiceProvider: URL, oauthParameters: String) {
+        self.authServiceProvider = authServiceProvider
+        self.oauthParameters = oauthParameters
     }
 }
